abbdfa9b4dff5e85383efeaf35d282ae13fae4ab
Bucky <bucky5683@gmail.com>
2024-01-31 16:51:42 +0900

    컴포넌트 동작추가

--- src/Counter.js
+++ src/Counter.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React , { useState } from 'react'; // 컴포넌트에서 동적인 값을 상태(state)라고 부릅니다. 리액트에 useState 라는 함수가 있는데요, 이것을 사용하면 컴포넌트에서 상태를 관리 할 수 있습니다.
 
 /*
     지금까지 우리가 리액트 컴포넌트를 만들 때는, 동적인 부분이 하나도 없었습니다. 값이 바뀌는 일이 없었죠.
     이번에는 컴포넌트에서 보여줘야 하는 내용이 사용자 인터랙션에 따라 바뀌어야 할 때 어떻게 구현할 수 있는지에 대하여 알아보겠습니다.
     리액트 16.8 이전 버전에서는 함수형 컴포넌트에서는 상태를 관리할 수 없었는데요, 리액트 16.8 에서 Hooks 라는 기능이 도입되면서 함수형 컴포넌트에서도 상태를 관리할 수 있게 되었습니다.
     이번에는 useState 라는 함수를 사용해보게 되는데, 이게 바로 리액트의 Hooks 중 하나입니다.
-
-    on이벤트이름={실행하고싶은함수}
 */
 
 function Counter() {
-    const onIncrease = () => {
-      console.log('+1')
-    }
-    const onDecrease = () => {
-      console.log('-1');
-    }
+
+  const [number, setNumber] = useState(0);  //useState 를 사용 할 때에는 상태의 기본값을 파라미터로 넣어서 호출해줍니다. 이 함수를 호출해주면 배열이 반환되는데요, 여기서 첫번째 원소는 현재 상태, 두번째 원소는 Setter 함수입니다.
+  /*
+    원래는 다음과 같이 해줘야 하지만
+    const numberState = useState(0);
+    const number = numberState[0];
+    const setNumber = numberState[1];
+    배열비구조화 할당을 통해 구현
+  */
+
+  const onIncrease = () => {
+    setNumber(prevNumber => prevNumber + 1);
+  }
+  const onDecrease = () => {
+    setNumber(prevNumber => prevNumber - 1);
+  }
+
   return (
     <div>
-      <h1>0</h1>
-      <button onClick={onIncrease}>+1</button>
+      <h1>{number}</h1>
+      <button onClick={onIncrease}>+1</button>  {/*on이벤트이름={실행하고싶은함수}*/}
       <button onClick={onDecrease}>-1</button>
     </div>
   );
