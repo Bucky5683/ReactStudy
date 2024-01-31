a195328367381bf3ff314ac3ff5d081de180a803
Bucky <bucky5683@gmail.com>
2024-01-31 13:59:44 +0900

    index js 설명 주석 추가

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// ReactDOM.render 의 역할은 브라우저에 있는 실제 DOM 내부에 리액트 컴포넌트를 렌더링하겠다는 것을 의미
+// id 가 root 인 DOM 을 선택하고 있는데, 이 DOM 이 어디있는지 볼까요?
+/*
+  public/index.html
+  <div id="root"></div>
+*/
+// 결국, 리액트 컴포넌트가 렌더링 될 때에는, 렌더링된 결과물이 위 div 내부에 렌더링되는 것 입니다.
+
 root.render(
   <React.StrictMode>
     <App />
