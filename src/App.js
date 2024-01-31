b9ceb3558b30de3496731e0e66c8c00053eed039
Bucky <bucky5683@gmail.com>
2024-01-31 13:56:38 +0900

    React 텍스트 작성해보기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import Hello from './Hello';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello/>  
+     {/* 컴포넌트는 일종의 UI 조각입니다. 그리고, 쉽게 재사용 할 수도 있습니다. */}
     </div>
   );
 }
